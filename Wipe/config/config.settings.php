e936422dd432147f5702019efd9254867e936c5e
Léo <69589034+Zomblard@users.noreply.github.com>
2022-10-29 11:15:33 +0200

    [ADD] compatible package

--- Wipe/config/config.settings.php
+++ Wipe/config/config.settings.php
@@ -12,16 +12,25 @@
 <?php
 
 return [
+   /* - - - - - - - - 
+       - - HEADER - -
+    - - - - - - - - - */
+    "header_active_title" => "true",
+    "header_active_logo" => "true",
+    "header_allow_register_button" => "true",
+    "header_allow_login_button" => "true",
+    "global_no_register_message" => "Nous somme désolé mais les inscriptions sont pour le moment désactiver.",
+
     /* - - - - - - - - 
        - - HOME - -
     - - - - - - - - - */
-
+    /*TITLE DESC*/
+    "home_title" => "Accueil",
     /*HERO SECTION*/
     "hero_title" => "Bienvenue !",
     "hero_description" => "Une super description pour mon serveur !",
     "hero_button_text" => "Texte du bouton",
     "hero_button_link" => "#",
-
     /*FEATURE SECTION*/
     "feature_section_active" => "true",
     "feature_section_title" => "Fonctionnalités",
@@ -31,16 +40,13 @@ return [
     "feature_description_2" => "Amet minim mollit non deserunt ullamco est sit aliqua",
     "feature_title_3" => "Réactif",
     "feature_description_3" => "Amet minim mollit non deserunt ullamco est sit aliqua",
-
     /*NEWS SECTION*/
     "news_section_active" => "true",
     "news_section_title" => "Nouveautés",
     "news_number_display" => "4",
-
     /*CONTACT SECTION*/
     "contact_section_active" => "true",
     "contact_section_title" => "Nous contacter",
-
     /*CUSTOM SECTION #1*/
     "custom_section_active_1" => "",
     "custom_section_title_1" => "Titre personnalisé 1",
@@ -57,6 +63,66 @@ return [
     /* - - - - - - - - 
        - - NEWS - -
     - - - - - - - - - */
+    "news_title" => "Nouveautés",
+    "news_description" => "Les dernières actus !",
     "news_page_title" => "Archives des nouveautés",
-    "news_page_number_display" => "20"
+    "news_page_number_display" => "20",
+
+    /* - - - - - - - - 
+       - - F.A.Q - -
+    - - - - - - - - - */
+    "faq_title" => "FAQ",
+    "faq_description" => "Retrouvez toutes les réponses à vos questions",
+    "faq_page_title" => "F.A.Q",
+    "faq_question_title" => "Une question ?",
+    "faq_answer_title" => "Des réponses",
+    "faq_display_autor" => "true",
+    "faq_display_form" => "true",
+
+    /* - - - - - - - - 
+       - - VOTES - -
+    - - - - - - - - - */
+    "vote_title" => "Votes",
+    "vote_description" => "Votez pour le serveur et gagnez des récompenses uniques !",
+    "votes_page_title" => "Voter",
+    "votes_participate_title" => "Participer",
+    "votes_top_10_title" => "Top 10 du mois",
+    "votes_top_10_global_title" => "Top 10 global",
+    "votes_display_global" => "true",
+
+    /* - - - - - - - - 
+       - - WIKI - -
+    - - - - - - - - - */
+    "wiki_title" => "Wiki",
+    "wiki_description" => "Découvrez notre wiki !",
+    "wiki_page_title" => "Wiki",
+    "wiki_menu_title" => "Navigation",
+    "wiki_display_categorie_icon" => "true",
+    "wiki_display_article_categorie_icon" => "true",
+    "wiki_display_article_icon" => "true",
+    "wiki_display_creation_date" => "true",
+    "wiki_display_edit_date" => "true",
+    "wiki_display_autor" => "true",
+
+    /* - - - - - - - - 
+       - - FOOTER - -
+    - - - - - - - - - */
+    "footer_year" => "2022",
+    "footer_open_link_new_tab" => "true",
+
+    "footer_link_facebook" => "#",
+    "footer_icon_facebook" => "fa-brands fa-facebook",
+    "footer_active_facebook" => "true",
+
+    "footer_link_twitter" => "#",
+    "footer_icon_twitter" => "fa-brands fa-square-twitter",
+    "footer_active_twitter" => "true",
+
+    "footer_link_instagram" => "#",
+    "footer_icon_instagram" => "fa-brands fa-instagram",
+    "footer_active_instagram" => "true",
+
+    "footer_link_discord" => "#",
+    "footer_icon_discord" => "fa-brands fa-discord",
+    "footer_active_discord" => "true",
 ];
