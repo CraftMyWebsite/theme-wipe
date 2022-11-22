c344a30445af1c80c87c071781b5ef50c9bf005f
Léo <69589034+Zomblard@users.noreply.github.com>
2022-11-22 18:31:50 +0100

    [FIX] default settings

--- Wipe/config/config.settings.php
+++ Wipe/config/config.settings.php
@@ -15,9 +15,9 @@ return [
    /* - - - - - - - - 
        - - HEADER & GLOBAL - -
     - - - - - - - - - */
-    "header_active_title" => "0",
-    "header_active_logo" => "0",
-    "header_allow_register_button" => "0",
+    "header_active_title" => "1",
+    "header_active_logo" => "1",
+    "header_allow_register_button" => "1",
     "header_allow_login_button" => "1",
     "global_no_register_message" => "Nous somme désolé mais les inscriptions sont pour le moment désactiver.",
 
