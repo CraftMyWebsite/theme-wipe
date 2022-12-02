b2741871dea38c774b87ab5e67f4868a2d3b43fe
Zomblard <leo.goigoux@gmail.com>
2022-12-02 16:51:41 +0100

    [ADD] history

---
+++ dev/views/shop/history.view.php
@@ -0,0 +1,42 @@
+
+
+<section class="bg-gray-800 relative text-white">
+    <img src="/dev/img/bg.webp" class="absolute h-full inset-0 object-center object-cover w-full"
+         alt="Vous devez upload bg.webp depuis votre panel !" width="1080" height="720"/>
+    <div class="container mx-auto px-4 py-12 relative">
+        <div class="flex flex-wrap -mx-4">
+            <div class="mx-auto px-4 text-center w-full lg:w-8/12">
+                <h1 class="font-extrabold mb-4 text-2xl md:text-6xl">Boutique</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-white rounded-lg shadow my-8 sm:mx-12 lg:mx-72">
+    <div class="container p-4">
+            <h3>Vos commandes</h3>
+
+        <table class="table" id="table1">
+            <thead>
+            <tr>
+                <th class="text-center">ss</th>
+                <th class="text-center">sss</th>
+                <th class="text-center">s</th>
+                <th class="text-center">sssss</th>
+                <th class="text-center">ss</th>
+            </tr>
+            </thead>
+            <tbody class="text-center">
+                <tr>
+                    <td class="text-center">12</td>
+                    <td class="text-center">123</td>
+                    <td class="text-center">123</td>
+                    <td class="text-center">123</td>
+                    <td class="text-center" class="btn btn-sm btn-primary"><i class="fa-solid fa-download"></i> Installer</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</section>
+
