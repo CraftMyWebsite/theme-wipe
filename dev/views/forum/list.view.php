c41a7465d457d6dcb6327b9f2f030501b68b1953
Zomblard <leo.goigoux@gmail.com>
2022-12-02 16:51:57 +0100

    [ADD] forum list view

---
+++ dev/views/forum/list.view.php
@@ -0,0 +1,75 @@
+<section class="bg-gray-800 relative text-white">
+    <img src="/dev/img/bg.webp" class="absolute h-full inset-0 object-center object-cover w-full"
+         alt="Vous devez upload bg.webp depuis votre panel !" width="1080" height="720"/>
+    <div class="container mx-auto px-4 py-12 relative">
+        <div class="flex flex-wrap -mx-4">
+            <div class="mx-auto px-4 text-center w-full lg:w-8/12">
+                <h1 class="font-extrabold mb-4 text-2xl md:text-6xl">Forum</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="grid grid-cols-4 gap-6 my-8 sm:mx-12 lg:mx-72">
+    <div class="col-span-3">
+
+        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-gray-700 bg-gray-50">
+                <tr>
+                    <th scope="col" class="py-3 px-6">
+                        Titre de catégorie
+                    </th>
+                    <th scope="col" class="text-center py-3 px-6">
+                        Topics
+                    </th>
+                    <th scope="col" class="text-center py-3 px-6">
+                        Messages
+                    </th>
+                    <th scope="col" class="text-center py-3 px-6">
+                        Dernier messages
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="text-center py-4 px-6">
+                        32
+                    </td>
+                    <td class="text-center py-4 px-6">
+                        967
+                    </td>
+                    <td class="py-4 px-6">
+
+                        <div class="flex text-sm">
+                            <a href="#">
+                                <div tabindex="0" class="avatar w-10">
+                                    <div class="w-fit rounded-full ">
+                                        <img src="https://placeimg.com/80/80/people">
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#">
+                                <div class="ml-2">
+                                    <div class="text-cmw-pink">Vous</div>
+                                    <div>Mardi 09 Juin, 19:42</div>
+                                </div>
+                            </a>
+                        </div>
+
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div class="bg-white rounded-lg shadow p-4 h-fit">
+        dd
+    </div>
+</section>
+
