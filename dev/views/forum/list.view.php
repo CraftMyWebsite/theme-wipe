e7886393c1debbac279c7f67163e4be80f7bc6fe
Zomblard <leo.goigoux@gmail.com>
2023-01-05 14:59:53 +0100

    [EDIT] forum list views

--- dev/views/forum/list.view.php
+++ dev/views/forum/list.view.php
@@ -12,60 +12,322 @@
 
 
 <section class="grid grid-cols-4 gap-6 my-8 sm:mx-12 lg:mx-72">
-    <div class="col-span-3">
+    <div class="lg:col-span-3 h-fit">
 
-        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500">
-                <thead class="text-gray-700 bg-gray-50">
-                <tr>
-                    <th scope="col" class="py-3 px-6">
-                        Titre de catégorie
-                    </th>
-                    <th scope="col" class="text-center py-3 px-6">
-                        Topics
-                    </th>
-                    <th scope="col" class="text-center py-3 px-6">
-                        Messages
-                    </th>
-                    <th scope="col" class="text-center py-3 px-6">
-                        Dernier messages
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
-                    </th>
-                    <td class="text-center py-4 px-6">
-                        32
-                    </td>
-                    <td class="text-center py-4 px-6">
-                        967
-                    </td>
-                    <td class="py-4 px-6">
 
-                        <div class="flex text-sm">
-                            <a href="#">
-                                <div tabindex="0" class="avatar w-10">
-                                    <div class="w-fit rounded-full ">
-                                        <img src="https://placeimg.com/80/80/people">
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#">
-                                <div class="ml-2">
-                                    <div class="text-cmw-pink">Vous</div>
-                                    <div>Mardi 09 Juin, 19:42</div>
-                                </div>
-                            </a>
+        <div class="w-full shadow-market mb-16">
+            <div class="flex py-4 bg-gray-100">
+                <div class="md:w-[55%] px-4 font-pop font-extrabold text-cmw-pink">Contenus importants</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Topics</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Messages</div>
+                <div class="hidden md:block w-[25%] font-pop font-extrabold text-center">Dernier messages</div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="subforum.php">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
                         </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Informations <span class="badge badge-success"> <i class="fa-solid fa-hand-sparkles mr-2"></i>NEW</span>
+                            </div>
+                            <div>
+                                Toutes les informations importantes liées à CraftMyWebsite.
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">32</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">967</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Vous</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="#">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Recrutement CMW [ON]
+                            </div>
+                            <div>
+                                CraftMyWebsite recrute des talents dans son équipe de passionnées, rejoins nous !
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">7</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">34</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Moi</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="#">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Mise a jour de CraftMyWebsite
+                            </div>
+                            <div>
+                                Découvre les correctifs et mises à jours du cms.
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">7</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">34</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Moi</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+
+
+
+
+        <div class="w-full shadow-market mb-16">
+            <div class="flex py-4 bg-gray-100">
+                <div class="md:w-[55%] px-4 font-pop font-extrabold text-cmw-pink">Zone communautaire</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Topics</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Messages</div>
+                <div class="hidden md:block w-[25%] font-pop font-extrabold text-center">Dernier messages</div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="subforum.php">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Suggestions / Boite à idées
+                            </div>
+                            <div>
+                                Vous avez une idée ? Une suggestion ? Alors postez-la ici !
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">32</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">967</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Vous</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="#">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Report de site hors-la-loi
+                            </div>
+                            <div>
+                                Il est temps de lever les armes contre ces malotru qui ne respecte pas la tendre et délicieuse CGU.
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">7</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">34</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Moi</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+        <div class="w-full shadow-market mb-16">
+            <div class="flex py-4 bg-gray-100">
+                <div class="md:w-[55%] px-4 font-pop font-extrabold text-cmw-pink">Entraide</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Topics</div>
+                <div class="hidden md:block w-[10%] font-pop font-extrabold text-center">Messages</div>
+                <div class="hidden md:block w-[25%] font-pop font-extrabold text-center">Dernier messages</div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="subforum.php">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Vos Tutoriels
+                            </div>
+                            <div>
+                                Postez ici dès maintenant vos tutoriels liés à CraftMyWebsite
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">32</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">967</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Vous</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex py-6 border-t border-cmw-black">
+                <div class="md:w-[55%] px-5 text-cmw-black">
+                    <a class="flex hover:text-cmw-pink" href="#">
+                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-cmw-white rounded-xl shadow-connect w-fit h-fit">
+                            <i class="text-h4 fa-solid fa-comment"></i>
+                        </div>
+                        <div class="ml-4">
+                            <div class="font-extrabold font-pop">
+                                Support
+                            </div>
+                            <div>
+                                Un problème ? Venez nous demander de l'aide ici !
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="hidden md:block w-[10%] font-pop text-center my-auto">7</div>
+                <div class="hidden md:inline-block w-[10%] font-pop text-center my-auto">34</div>
+                <!--Dernier message-->
+                <div class="hidden md:block w-[25%] my-auto">
+                    <div class="flex text-sm">
+                        <a href="#">
+                            <div tabindex="0" class="avatar w-10">
+                                <div class="w-fit rounded-full ">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="ml-2">
+                                <div class="text-cmw-pink">Moi</div>
+                                <div>Mardi 09 Juin, 19:42</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+
+
 
     </div>
     <div class="bg-white rounded-lg shadow p-4 h-fit">
