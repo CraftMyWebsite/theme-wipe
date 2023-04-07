80df0eff0df970b4ce8d76956b9277d12aafe7bc
Zomblard <leo.goigoux@gmail.com>
2023-04-07 17:40:07 +0200

    [EDIT] Forum

--- dev/views/forum/forum.view.php
+++ dev/views/forum/forum.view.php
@@ -98,29 +98,25 @@
                 <div class="hidden md:block w-[25%] font-bold text-center">Dernier messages</div>
             </div>
 
-            <div class="flex py-6 border-t bg-gray-50 hover:bg-gray-100">
-                <div class="md:w-[55%] px-5">
-                    <a class="flex flex-wrap" href="subforum.php">
-                        <div class="py-2 px-2 bg-gradient-to-b from-gray-400 to-gray-300 rounded-xl shadow-connect w-fit h-fit">
-                            <i class="fa-solid fa-comment fa-xl"></i>
+            <div class="relative flex py-2 border-t bg-gray-50 hover:bg-gray-100">
+                <div class="md:w-[55%] px-5 relative">
+                    <a class="flex flex-wrap hover:text-blue-800" href="subforum.php">
+                        <div class="w-12 shadow-xl">
+                            <img src="https://randomuser.me/api/portraits/women/33.jpg" />
                         </div>
                         <div class="ml-4">
-                            <div class="font-bold">
-                                Je suis un fifou
-                            </div>
-                            <div>
-                                <b>Zomb</b> le
-                            </div>
+                            <p>Je suis un fifou qui fait un super</p>
+                            <p><span class="font-medium">Zomb</span> <span class="text-sm">le </span></p>
+                            </p>
                         </div>
-                        <div class="ml-auto">
+                        <div class="absolute top-0 right-0">
                             <i data-tooltip-target="tooltip-pined" class="fa-solid fa-thumbtack fa-sm text-red-600"></i>
                             <div id="tooltip-pined" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
-                                Topic important
+                                Important
                             </div>
-
                             <i data-tooltip-target="tooltip-closed" class="fa-solid fa-lock fa-sm text-yellow-300 ml-6"></i>
                             <div id="tooltip-closed" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
-                                Topic clos
+                                Fermé
                             </div>
                         </div>
                     </a>
@@ -132,8 +128,8 @@
                     <div class="flex text-sm">
                         <a href="#">
                             <div tabindex="0" class="avatar w-10">
-                                <div class="w-fit rounded-full ">
-                                    <img src="https://placeimg.com/80/80/people" />
+                                <div class="w-fit">
+                                    <img src="https://randomuser.me/api/portraits/women/33.jpg" />
                                 </div>
                             </div>
                         </a>
@@ -145,10 +141,60 @@
                         </a>
                     </div>
                 </div>
+
+                <!------------------
+                 -- ADMIN SECTION --
+                -------------------->
+                <i data-modal-target="defaultModal" data-modal-toggle="defaultModal" data-tooltip-target="tooltip-admin" class="absolute right-1 top-3 fa-solid fa-lg fa-screwdriver-wrench text-blue-800 ml-6 "></i>
+                <div id="tooltip-admin" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                    Administration du topic
+                </div>
+                <!------------------
+                 --- ADMIN MODAL ---
+                -------------------->
+                <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                    <div class="relative w-full h-full max-w-2xl md:h-auto">
+                        <!-- Modal content -->
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <!-- Modal header -->
+                            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Gestion de
+                                </h3>
+                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="p-4">
+
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Déplacer vers</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>Ne pas déplacer</option>
+                                    <option>Un autre topic</option>
+                                </select>
+
+
+                            </div>
+                            <!-- Modal footer -->
+                            <div class="flex justify-between p-6 space-x-2 border-t border-gray-200 rounded-b">
+                                <button type="button" class="text-gray-700 border-2 border-red-700 hover:border-red-800 font-medium rounded-md text-sm px-2 py-2.5 mr-2 mb-2"><i class="fa-solid fa-trash fa-lg"></i> Corbeille</button>
+                                <button type="button" class="text-gray-700 border-2 border-blue-600 hover:border-blue-800 font-medium rounded-md text-sm px-2 py-2.5 mr-2 mb-2"><i class="fa-solid fa-thumbtack text-red-600 fa-lg"></i> Épingler</button>
+                                <button type="button" class="text-gray-700 border-2 border-blue-600 hover:border-blue-800 font-medium rounded-md text-sm px-2 py-2.5 mr-2 mb-2"><i class="fa-solid fa-lock text-yellow-300 fa-lg"></i> Clore</button>
+                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-md text-sm px-2 py-2.5 mr-2 mb-2">Valider</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
+
+
+
         </div>
     </div>
-
 
 
     <div class="h-fit">
@@ -177,3 +223,11 @@
         </div>
     </div>
 </section>
+
+
+
+
+
+
+
+
