463d3bc9c4141ed1b4ea73f65d2a35880ea6d6d6
Léo <69589034+Zomblard@users.noreply.github.com>
2023-04-08 23:32:07 +0200

    [ADD] like / dislike / delete features

--- Wipe/views/forum/topic.view.php
+++ Wipe/views/forum/topic.view.php
@@ -3,6 +3,7 @@
 use CMW\Model\Core\ThemeModel;
 use CMW\Utils\Utils;
 use CMW\Manager\Security\SecurityManager;
+use CMW\Controller\Users\UsersController;
 
 $title = "Titre de la page";
 $description = "Description de votre page";
@@ -80,18 +81,24 @@ $description = "Description de votre page";
             <div class="flex justify-between">
                 <h4><?= $topic->getName() ?></h4>
                 <div class="">
-                    <i data-tooltip-target="tooltip-important" class="fa-solid fa-triangle-exclamation text-orange-500 ml-4"></i>
-                    <div id="tooltip-important" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
-                        Important
-                    </div>
-                    <i data-tooltip-target="tooltip-pined" class="fa-solid fa-thumbtack text-red-600 ml-4"></i>
-                    <div id="tooltip-pined" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
-                        Épinglé
-                    </div>
-                    <i data-tooltip-target="tooltip-closed" class="fa-solid fa-lock text-yellow-300 ml-4"></i>
-                    <div id="tooltip-closed" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
-                        Fermé
-                    </div>
+                    <?php if($topic->isImportant()): ?>
+                        <i data-tooltip-target="tooltip-important" class="fa-solid fa-triangle-exclamation text-orange-500 ml-4"></i>
+                        <div id="tooltip-important" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                            Important
+                        </div>
+                    <?php endif; ?>
+                    <?php if($topic->isPinned()): ?>
+                        <i data-tooltip-target="tooltip-pined" class="fa-solid fa-thumbtack text-red-600 ml-4"></i>
+                        <div id="tooltip-pined" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                            Épinglé
+                        </div>
+                    <?php endif; ?>
+                    <?php if($topic->isDisallowReplies()): ?>
+                        <i data-tooltip-target="tooltip-closed" class="fa-solid fa-lock text-yellow-300 ml-4"></i>
+                        <div id="tooltip-closed" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                            Fermé
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
 
@@ -155,9 +162,6 @@ $description = "Description de votre page";
             </div>
         </section>
 
-        <?php if (!$responseModel->countResponseInTopic($topic->getId())): ?>
-    <h4 style="text-align: center">Soyez le premier à répondre !</h4>
-<?php endif; ?>
 <?php foreach ($responseModel->getResponseByTopic($topic->getId()) as $response) : ?>
 <section class="border mt-4">
             <div class="flex justify-between bg-gray-200 p-2">
@@ -202,23 +206,56 @@ $description = "Description de votre page";
                             <p>#1</p>
                         </div>
                     </div>
+
+
+                    <div class="flex justify-end p-1">
+                        <?php if ($response->isSelfReply()): ?>
+                        <i data-modal-target="popup-modal" data-modal-toggle="popup-modal" data-tooltip-target="tooltip-delete" class="fa-solid fa-trash text-red-500 ml-4"></i>
+                        <div id="tooltip-delete" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                            Supprimer ma réponse
+                        </div>
+                        <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                            <div class="relative w-full h-full max-w-md md:h-auto">
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                    <div class="p-6 text-center">
+                                        <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Voulez-vous vraiment supprimer votre réponse ?</h3>
+                                        <a href="<?= $response->deleteLink() ?>" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                            Supprimer
+                                        </a>
+                                        <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Annuler</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php if (UsersController::isUserLogged() && !$response->isSelfReply()): ?>
+                        <div class="ml-4">
+                            0 <i data-tooltip-target="tooltip-like" class="fa-solid fa-thumbs-up"></i>
+                            <div id="tooltip-like" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                                Like
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            0 <i data-tooltip-target="tooltip-dislike" class="fa-solid fa-thumbs-down"></i>
+                            <div id="tooltip-dislike" role="tooltip" class="absolute z-10 invisible inline-block p-2 text-sm font-medium text-white bg-gray-700 rounded-lg">
+                                Dislike
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
-                <?php if ($response->isSelfReply()): ?>
-                    <a href="<?= $response->deleteLink() ?>">Supprimer ma réponse</a>
-                <?php endif; ?>
             </div>
         </section>
 <?php endforeach; ?>
 
 
 
-<?php if($topic->isDisallowReplies()): ?>
-
-    <section>
-        Ce topic est clos
-    </section>
-
-<?php else: ?>
+<?php if(!$topic->isDisallowReplies() && UsersController::isUserLogged()): ?>
         <section class="border mt-4">
             <div class="bg-gray-200 p-2">
                 <p><b>Répondre à ce topic :</b></p>
@@ -241,11 +278,11 @@ $description = "Description de votre page";
                             <div class="flex justify-end mt-2">
                                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"><i class="fa-solid fa-reply"></i> Poster ma réponse</button>
                             </div>
+                        </form>
                         </div>
                     </div>
-                </form>
             </div>
         </section>
-    </div>
-</section>
 <?php endif; ?>
+</div>
+</section>
