0af0c1935187b74dcbd47ca787545baa9f2c85dd
Léo <69589034+Zomblard@users.noreply.github.com>
2023-06-14 18:27:07 +0200

    [ADD] move topic to another forum or subforum

--- Wipe/Views/Forum/forum.view.php
+++ Wipe/Views/Forum/forum.view.php
@@ -360,10 +360,17 @@ $description = "Description de votre page";
                                                     <label for="countries"
                                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Déplacer
                                                         vers :</label>
-                                                    <select id="countries"
+                                                    <select id="countries" name="move"
                                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                                        <option selected>Ne pas déplacer</option>
-                                                        <option>Un autre topic</option>
+                                                        <?php foreach ($categoryModel->getCategories() as $cat): ?>
+                                                            <option disabled>──── <?= $cat->getName() ?> ────</option>
+                                                            <?php foreach ($forumModel->getForumByCat($cat->getId()) as $forumObject): ?>
+                                                                <option value="<?= $forumObject->getId() ?>" <?= ($forumObject->getName() === $topic->getForum()->getName() ? "selected" : "") ?>><?= $forumObject->getName() ?></option>
+                                                                <?php foreach ($forumModel->getSubforumByForum($forumObject->getId()) as $subForumObject): ?>
+                                                                    <option value="<?= $subForumObject->getId() ?>">↪ <?= $subForumObject->getName() ?></option>
+                                                                <?php endforeach; ?>
+                                                            <?php endforeach; ?>
+                                                        <?php endforeach; ?>
                                                     </select>
                                                 </div>
                                             </div>
