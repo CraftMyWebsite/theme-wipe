5816554f850cc9e3550b3e5407272b51384a9d11
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-09 12:58:31 +0200

    [Add] add to cart link

--- Views/Shop/item.view.php
+++ Views/Shop/item.view.php
@@ -1,5 +1,7 @@
 <?php
 
+use CMW\Manager\Env\EnvManager;
+use CMW\Model\Users\UsersModel;
 use CMW\Utils\Website;
 
 $title = Website::getName() . ' - Shop';
@@ -7,3 +9,4 @@ $description = 'Visitez notre shop ';
 
 ?>
 Item
+<a href="<?=Website::getProtocol()?>://<?=$_SERVER["SERVER_NAME"]?><?=EnvManager::getInstance()->getValue("PATH_SUBFOLDER")?>shop/add_to_cart/36/<?= UsersModel::getCurrentUser()->getId() ?>">Ajouter au panier</a>
