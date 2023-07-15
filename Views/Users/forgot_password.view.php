e2ce6b2229cd7c804a311afdd9efa914084a4064
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-15 19:17:58 +0200

    [Add] Captcha forgot password

--- Views/Users/forgot_password.view.php
+++ Views/Users/forgot_password.view.php
@@ -1,5 +1,6 @@
 <?php
 
+use CMW\Controller\Core\SecurityController;
 use CMW\Manager\Env\EnvManager;
 use CMW\Manager\Lang\LangManager;
 use CMW\Manager\Security\SecurityManager;
@@ -36,6 +37,7 @@ $description = "C'est pas très bien d'oublié son mot de passe ...";
                     <a href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>register" class="text-sm text-blue-700 hover:underline dark:text-blue-500">S'inscrire</a>
                     <a href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>login" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Connexion</a>
                 </div>
+                <?php SecurityController::getPublicData(); ?>
                 <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Envoyer</button>
             </form>
         </div>
