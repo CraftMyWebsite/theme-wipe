5aee2a9d6f7b1c5b6538c4416ca7c0365c995caa
Zomb <leo.goigoux@gmail.com>
2024-03-29 16:49:12 +0100

    [IMPROVE] input cart placeholder

--- Views/Shop/Cart/cart.view.php
+++ Views/Shop/Cart/cart.view.php
@@ -118,7 +118,7 @@ Website::setDescription("Votre panier");
                                     <?php (new SecurityManager())->insertHiddenToken() ?>
                                     <input type="text" autocomplete="off" name="code"
                                        class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                                       placeholder="Code promo">
+                                       placeholder="Code promo / Carte cadeau">
                                     <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                         Appliquer
                                     </button>
