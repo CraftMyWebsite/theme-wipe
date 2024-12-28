3dc59124f9105c146dd0587d081ae8c9eecc9a7c
Teyir <60007549+Teyir@users.noreply.github.com>
2024-12-28 20:32:32 +0100

    0.0.4

--- Theme.php
+++ Theme.php
@@ -13,7 +13,7 @@ class Theme implements IThemeConfig
 
     public function version(): string
     {
-        return '0.0.3';
+        return '0.0.4';
     }
 
     public function cmwVersion(): string
