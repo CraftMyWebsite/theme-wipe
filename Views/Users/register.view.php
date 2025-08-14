a524157fe97957ba877b9b5eb5b832ab8803e95f
zomb <leo.goigoux@gmail.com>
2025-08-14 13:19:55 +0200

    [ADD] Accept Terms checkbox

--- Views/Users/register.view.php
+++ Views/Users/register.view.php
@@ -1,15 +1,18 @@
 <?php
 
 use CMW\Controller\Core\SecurityController;
+use CMW\Interface\Users\IUsersOAuth;
 use CMW\Manager\Lang\LangManager;
 use CMW\Manager\Security\SecurityManager;
 use CMW\Model\Core\ThemeModel;
 use CMW\Utils\Website;
 
+/* @var IUsersOAuth[] $oAuths */
+/* @var string $needTextTerms */
+/* @var bool $needTerms */
+
 Website::setTitle('Inscription');
 Website::setDescription('Inscrivez-vous');
-
-/* @var \CMW\Interface\Users\IUsersOAuth[] $oAuths */
 ?>
 
 <section class="bg-gray-800 relative text-white">
@@ -69,6 +72,17 @@ Website::setDescription('Inscrivez-vous');
                                 <i class="fa fa-eye-slash"></i></div>
                         </div>
                     </div>
+                    <?php if ($needTerms): ?>
+                        <div style="display:flex; justify-content: space-between; margin-top: 10px">
+                            <div style="display: flex; align-items: center">
+                                <div class="flex items-center h-5">
+                                    <input id="acceptTerms" name="acceptTerms" type="checkbox" value=""
+                                           class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
+                                </div>
+                                <label for="acceptTerms" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"><?= $needTextTerms ?></label>
+                            </div>
+                        </div>
+                    <?php endif; ?>
 
                     <?php SecurityController::getPublicData(); ?>
 
