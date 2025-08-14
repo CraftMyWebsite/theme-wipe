79886c6890e3ee3b29c308b44b685f29941ec080
zomb <leo.goigoux@gmail.com>
2025-08-14 13:01:47 +0200

    [FIX] mistakes to "

--- Views/ShopExtendedToken/main.view.php
+++ Views/ShopExtendedToken/main.view.php
@@ -43,7 +43,7 @@ Website::setDescription('Gérez vos ' . $token->name());
                 <div class="mb-4">
                     <label for="amount" class="block mb-2 text-sm font-medium text-gray-900">Montant :</label>
                     <input type="text" name="amount" id="amount" placeholder="19.99"
-                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5""
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     required>
                 </div>
                 <div class="mb-2">
